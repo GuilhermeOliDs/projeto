c6a5d2ac5a7f76ccb9f8c92020810f91f5ce9832
Guilherme Henrique de Oliveira <149117151+GuilhermeOliDs@users.noreply.github.com>
2024-04-02 16:39:27 -0300

    Update extraction.py

--- src/extraction.py
+++ src/extraction.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def load_data():
-    return pd.read_csv("C:\\Users\\Guilherme\\repos\\projeto_git\\projeto-main\\data\\processed\\bikes_completed.csv")
+    return pd.read_csv("C:\\Users\\Guilherme\\repos\\projeto_git\\projeto-main\\data\\bikes_completed.csv")
 
